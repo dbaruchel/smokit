71afe98064f380dd9327a0c828b082368238a23a
mcoenca <m.coenca@gmail.com>
2014-02-18 00:28:34 +0100

    LocalStorage, page dynamique en javascript pour se logger

--- public/script.js
+++ public/script.js
@@ -1,23 +1,62 @@
 //prend ce qu'il y a dans le formulaire et l'envoie dans localStorage à l'id 'userName' Possibilité de trucs JSON
-
 var uName=localStorage.getItem('userName');
-
 //Si la variableuName est définie, on l'affiche en haut, sinon(à faire) on affiche le formulaire
-if (uName!=null) {
+if (uName!=null && uName!='') {
 var titre=document.createElement('h1'),
     titreText=document.createTextNode(uName);
     titre.appendChild(titreText);
    // document.body.appendChild(titre); Met en dernier
-   document.body.insertBefore(titre,document.body.firstChild);  
+   document.body.insertBefore(titre,document.body.firstChild);
+  	
+var form=document.createElement('form'),
+	inputCreer=document.createElement('input');
+	
+	form.id='myForm';
+	inputCreer.type='submit';
+	inputCreer.value='Autre utilisateur ?';
+	inputCreer.setAttribute("onclick","resetStorage();");
+	inputCreer.onclick=resetStorage;		
+	form.appendChild(inputCreer);
+	document.body.appendChild(form);
+   
+}
+else {
+	var form=document.createElement('form'),
+	label=document.createElement('label'),
+	labelText=document.createTextNode('Entrez Username :'),
+	span=document.createElement('span'),
+	inputUser=document.createElement('input'),
+	inputCreer=document.createElement('input');
+	form.id='myForm';
+	label.className='form_col';
+	label.htmlFor='userName';
+	inputUser.name='userName';
+	inputUser.id='userName';
+	inputUser.type='text';
+	span.className='form_col';
+	inputCreer.type='submit';
+	inputCreer.value='M inscrire';
+	inputCreer.setAttribute("onclick","getUserName();");
+	inputCreer.onclick=getUserName;
+	label.appendChild(labelText);
+	form.appendChild(label);
+	form.appendChild(inputUser);
+	form.appendChild(span);
+	form.appendChild(inputCreer);
+	document.body.appendChild(form);
 }
 
 function getUserName() {
 var userName = document.getElementById('userName').value;
 localStorage.setItem('userName', userName);
-alert(userName);
-var retrieved = localStorage.getItem('userName');
-alert(retrieved);
+//alert(userName);
+//var retrieved = localStorage.getItem('userName');
+//alert(retrieved);
 
 //console.log('retrieved: ', JSON.parse(retrieved)); Vu sur stackOverflow mais, A quoi ca sert ??
+}
+
+function resetStorage() {
+localStorage.clear();
 }
 
