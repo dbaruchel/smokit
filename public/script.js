c010ab747c87d3246ac30abc10f6887c453c40f6
mcoenca <m.coenca@gmail.com>
2014-02-17 15:48:01 +0100

    Debut de localStorage et de page dynamique javascript

--- public/script.js
+++ public/script.js
@@ -1,2 +1,23 @@
-alert('Hello World');
+//prend ce qu'il y a dans le formulaire et l'envoie dans localStorage à l'id 'userName' Possibilité de trucs JSON
+
+var uName=localStorage.getItem('userName');
+
+//Si la variableuName est définie, on l'affiche en haut, sinon(à faire) on affiche le formulaire
+if (uName!=null) {
+var titre=document.createElement('h1'),
+    titreText=document.createTextNode(uName);
+    titre.appendChild(titreText);
+   // document.body.appendChild(titre); Met en dernier
+   document.body.insertBefore(titre,document.body.firstChild);  
+}
+
+function getUserName() {
+var userName = document.getElementById('userName').value;
+localStorage.setItem('userName', userName);
+alert(userName);
+var retrieved = localStorage.getItem('userName');
+alert(retrieved);
+
+//console.log('retrieved: ', JSON.parse(retrieved)); Vu sur stackOverflow mais, A quoi ca sert ??
+}
 
