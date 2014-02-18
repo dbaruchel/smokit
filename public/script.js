3a97232527eff1b27d713f71ccc0a844c9562bca
mcoenca <m.coenca@gmail.com>
2014-02-18 16:59:16 +0100

    Debut de script xhr

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 //prend ce qu'il y a dans le formulaire et l'envoie dans localStorage à l'id 'userName' Possibilité de trucs JSON
 var uName=localStorage.getItem('userName');
-//Si la variableuName est définie, on l'affiche en haut, sinon(à faire) on affiche le formulaire
+//Si la variableuName est définie, on l'affiche en haut, sinon on affiche le formulaire
 if (uName!=null && uName!='') {
 var titre=document.createElement('h1'),
     titreText=document.createTextNode(uName);
@@ -18,8 +18,15 @@ var form=document.createElement('form'),
 	inputCreer.onclick=resetStorage;		
 	form.appendChild(inputCreer);
 	document.body.appendChild(form);
-   
+
+//Requete XMLHttpRequest en GET
+var xhr = getXMLHttpRequest();
+//Avant de passer des variables, faire
+//var sVar=encodeURIComponent("Contenu avec des espaces");
+xhr.open("GET","/users/2", true);
+xhr.send(null);
 }
+
 else {
 	var form=document.createElement('form'),
 	label=document.createElement('label'),
@@ -49,6 +56,8 @@ else {
 function getUserName() {
 var userName = document.getElementById('userName').value;
 localStorage.setItem('userName', userName);
+
+//Tests du localStorage
 //alert(userName);
 //var retrieved = localStorage.getItem('userName');
 //alert(retrieved);
@@ -58,5 +67,25 @@ localStorage.setItem('userName', userName);
 
 function resetStorage() {
 localStorage.clear();
+}
+
+function getXMLHttpRequest() {
+	var xhr = null;
+
+	if (window.XMLHttpRequest || window.ActiveXObject){
+		if (window.ActiveXObject) {
+			try {
+			xhr = new ActiveXObject("Masxml2.XMLHTTP");
+			} catch (e) {
+			xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest();
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	return xhr;
 }
 
